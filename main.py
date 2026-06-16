aba85511457693a937d3e21967f1c0c371e2e929
Matías Joaquín Tucci <mtucci@millsproducts.com>
2026-06-16 15:54:56 -0600

    Clean Architechture implemented

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
-# Comments
+# Video 1
 # Initiate the project (creates folder)
 # uv init fastapi_blog_true
-# Adding fastapi with STANDARD
+# Adding fastapi with STANDARD adds jinja2
 # uv add "fastapi[standard]"
 # Run application
 # uv run fastapi dev main.py
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse # For HTML responses, not needed for API only
+# Video 2
+# Jinja2 Templates
+# Templates = dynamic data
+# Past data to templates
+# cicles for templates
+# Standard CSS for the html
+# pip install jinja2
+# uv add jinja2 #This is in case you didn't used [standard] when installing fasapi
+
+from fastapi import FastAPI, Request
+#from fastapi.responses import HTMLResponse # For HTML responses, not needed for API only
+from fastapi.templating import Jinja2Templates # For HTML responses, not needed for API only
+# To add static files
+from fastapi.staticfiles import StaticFiles
+
+# Clean Architecture modifications, adding client
+from posts.client import PostClient
+from posts import controller, controllerhtml
 
 app = FastAPI()
 
-posts: list[dict] = [
-    {
-        "id": 1,
-        "author": "Corey Schafer",
-        "title": "FastAPI is Awesome",
-        "content": "This framework is really easy to use and super fast.",
-        "date_posted": "April 20, 2025",
-    },
-    {
-        "id": 2,
-        "author": "Jane Doe",
-        "title": "Python is Great for Web Development",
-        "content": "Python is a great language for web development, and FastAPI makes it even better.",
-        "date_posted": "April 21, 2025",
-    },
-]
-
-@app.get("/"
-         , response_class=HTMLResponse # For HTML responses, not needed for API only
-         , include_in_schema= False
-         )
-@app.get("/posts"
-         , response_class=HTMLResponse # For HTML responses, not needed for API only
-         , include_in_schema= False
-         )
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(controller.router, prefix="/api/v1/posts", tags=["Post"])
+app.include_router(controllerhtml.router, tags=["Post"])
 
 
-@app.get("/api/posts")
-def get_posts():
-    return posts
+#
+#@app.get("/api/posts/{id}")
+#def get_post(id: int):
+#    return next((p for p in posts if p["id"] == id), None)
+
+
